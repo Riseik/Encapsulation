90e22df4d1dfb8fd38dfad012b821efbca1d903b
joachim-gabin <ejoachimgabin@gaming.tech>
2024-11-19 11:56:08 +0100

    start SpriteSDL
    start SpriteSDL

--- Encapsulation/SpriteSDL.cpp
+++ Encapsulation/SpriteSDL.cpp
@@ -1,1 +1,6 @@
 #include "SpriteSDL.h"
+
+void SpriteSDL::LoadSprite()
+{
+	tmpSurface = SDL_LoadBMP("../SCP.png");
+}
--- Encapsulation/SpriteSDL.h
+++ Encapsulation/SpriteSDL.h
@@ -1,8 +1,11 @@
 #pragma once
-
 #include "Sprite.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_test_images.h>
 
 class SpriteSDL : public Sprite
 {
+	void LoadSprite() override;
+	SDL_Surface* tmpSurface;
 };
 
